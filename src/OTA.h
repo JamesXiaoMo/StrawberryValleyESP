a3b1461e51c9b85f635c2266f4e88f3f2fbfdad3
JamesXiaoMo <jameswuxiaomo@gmail.com>
2024-06-16 15:13:27 +0900

    Update

--- src/OTA.h
+++ src/OTA.h
@@ -1,41 +1,63 @@
 #include <HTTPClient.h>
 #include <Update.h>
 
+const char* OTA_request_url = "http://192.168.0.116:5000/check_version";
+const char* OTA_upgrade_url = "http://192.168.0.116:5000/upgrade/";
 
-const char* OTA_url = ""
 
-void OTA(){
+void OTA_update(String version){
+    Serial.println("Start upgrading version:" + version);
     HTTPClient http;
-    http.begin(firmware_url);
+    http.begin(OTA_upgrade_url + version);
     int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK) {
-      int contentLength = http.getSize();
-      bool canBegin = Update.begin(contentLength);
-      if (canBegin) {
-        WiFiClient* client = http.getStreamPtr();
-        size_t written = Update.writeStream(*client);
-        if (written == contentLength) {
-          Serial.println("Written : " + String(written) + " successfully");
-          if (Update.end()) {
-            Serial.println("OTA done!");
-            if (Update.isFinished()) {
-              Serial.println("Update successfully completed. Rebooting.");
-              ESP.restart();
+        int contentLength = http.getSize();
+        bool canBegin = Update.begin(contentLength);
+        if (canBegin) {
+            WiFiClient* client = http.getStreamPtr();
+            size_t written = Update.writeStream(*client);
+            if (written == contentLength) {
+                Serial.println("Written : " + String(written) + " successfully");
+                if (Update.end()) {
+                    Serial.println("OTA done!");
+                    if (Update.isFinished()) {
+                        Serial.println("Update successfully completed. Rebooting.");
+                        ESP.restart();
+                        } else {
+                            Serial.println("Update not finished? Something went wrong!");
+                        }
+                    } else {
+                        Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+                    }
+                } else {
+                    Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
+                }
             } else {
-              Serial.println("Update not finished? Something went wrong!");
+                Serial.println("Not enough space to begin OTA");
             }
-          } else {
-            Serial.println("Error Occurred. Error #: " + String(Update.getError()));
-          }
         } else {
-          Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
+        Serial.println("Error on HTTP request");
         }
+  http.end();
+}
+
+void OTA_request(String current_version){
+    HTTPClient http;
+    http.begin(OTA_request_url);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String postData = "version=" + current_version;
+    int httpCode = http.POST(postData);
+
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println("Server response: " + payload);
+      if (payload == "NO_UPDATE") {
+        Serial.println("No update needed.");
       } else {
-        Serial.println("Not enough space to begin OTA");
+        OTA_update(payload);
       }
     } else {
       Serial.println("Error on HTTP request");
     }
     http.end();
-  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 #include <Arduino.h>
 #include <Wi-Fi.h>
 #include <Ticker.h>
+#include <OTA.h>
+
+const char* current_version = "1.0.0";
 
 Ticker WiFiCheckTimeer(CheckWiFi, 60000);
 Ticker SocketCheckTimer(CheckSocket, 65000);
+Ticker OTACheckTimer([]() {OTA_request(current_version);}, 60000);
 
 void setup(){
   Serial.begin(115200);
   WiFiSetup();
   WiFiCheckTimeer.start();
+  //OTA_request(current_version);
   //TcpSocketSetup();
   //SocketCheckTimer.start();
+  OTACheckTimer.start();
   addRelay(0, 15);
   addRelay(1, 16);
 }
 
 void loop(){
   WiFiCheckTimeer.update();
+  OTACheckTimer.update();
   //SocketCheckTimer.update();
   //RecvData();
 }
