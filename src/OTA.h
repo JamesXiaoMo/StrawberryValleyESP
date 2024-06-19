aaa5b1cd5ed0f131a331aff11ef0b268cd095aaa
JamesXiaoMo <jameswuxiaomo@gmail.com>
2024-06-20 00:46:16 +0900

    update

--- src/OTA.h
+++ src/OTA.h
@@ -2,18 +2,22 @@
 #include <Update.h>
 
 const char* OTA_request_url = "http://192.168.0.116:5000/check_version";
-const char* OTA_upgrade_url = "http://192.168.0.116:5000/upgrade/";
+const char* OTA_upgrade_url = "http://192.168.0.116:5000/check_upgrade/";
 
 
-void OTA_update(String version){
-    Serial.println("Start upgrading version:" + version);
+void OTA_update(String current_version, String project){
     HTTPClient http;
-    http.begin(OTA_upgrade_url + version);
+    http.begin(OTA_upgrade_url + project + "/" + current_version);
     int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK) {
-        int contentLength = http.getSize();
-        bool canBegin = Update.begin(contentLength);
-        if (canBegin) {
+        String payload = http.getString();
+        Serial.println("Server response: " + payload);
+        if (payload == "NO_UPDATE") {
+            Serial.println("No update needed.");
+        } else {
+            int contentLength = http.getSize();
+            bool canBegin = Update.begin(contentLength);
+            if (canBegin) {
             WiFiClient* client = http.getStreamPtr();
             size_t written = Update.writeStream(*client);
             if (written == contentLength) {
@@ -35,29 +39,8 @@ void OTA_update(String version){
             } else {
                 Serial.println("Not enough space to begin OTA");
             }
-        } else {
-        Serial.println("Error on HTTP request");
+        }
+            Serial.println("Error on HTTP request");
         }
   http.end();
-}
-
-void OTA_request(String current_version){
-    HTTPClient http;
-    http.begin(OTA_request_url);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    String postData = "version=" + current_version;
-    int httpCode = http.POST(postData);
-
-    if (httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-      Serial.println("Server response: " + payload);
-      if (payload == "NO_UPDATE") {
-        Serial.println("No update needed.");
-      } else {
-        OTA_update(payload);
-      }
-    } else {
-      Serial.println("Error on HTTP request");
-    }
-    http.end();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include <OTA.h>
 
 const char* current_version = "1.0.0";
+const char* project = "StrawberryValleyProject";
 
 Ticker WiFiCheckTimeer(CheckWiFi, 60000);
 Ticker SocketCheckTimer(CheckSocket, 65000);
-Ticker OTACheckTimer([]() {OTA_request(current_version);}, 60000);
+Ticker OTACheckTimer([]() {OTA_update(current_version, project);}, 60000);
 
 void setup(){
   Serial.begin(115200);
@@ -19,6 +20,7 @@ void setup(){
   OTACheckTimer.start();
   addRelay(0, 15);
   addRelay(1, 16);
+  OTA_update(current_version, project);
 }
 
 void loop(){
